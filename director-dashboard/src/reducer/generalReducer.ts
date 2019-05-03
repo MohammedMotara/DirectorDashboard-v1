3c9777824b9e4fe4b96f3e0f67940d585b0f7729
Mohammed Motara <mohammedmotara@Mohammeds-MacBook.local>
2019-05-03 17:44:22 +0100

    Setting up redux reducer

---
+++ director-dashboard/src/reducer/generalReducer.ts
@@ -0,0 +1,15 @@
+// Action Types
+
+export const CHANGE_COMP_NAME = "CHANGE_COMP_NAME"
+
+// Action Creators
+
+
+
+// Action type Interfaces
+
+// export interface IGeneralState {
+//     pageName: string;
+// }
+
+// Reducer and initial State object - This is where the Switch Statement goes
